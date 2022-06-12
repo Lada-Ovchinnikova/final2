608e427f13b8ff63d9ff46a340039986a6115b3c
Lada-Ovchinnikova <lada.ovchinnikova@mail.ru>
2022-06-13 01:08:12 +0300

    test1

---
+++ controllers/cartcontroller.php
@@ -0,0 +1,46 @@
+<?php
+
+class CartController {
+
+    private $cartModel;
+    public $isAuthorized;
+    public $isAdmin;
+
+    public function __construct()
+    {
+        $this->cartModel = new Cart();
+        $this->isAuthorized = (new User())->userIsAuthorized();
+        $this->isAdmin = (new User())->userIsAdmin();
+
+    }
+
+    public function actionIndex()
+    {
+        $title = 'Корзина';
+        $items = $this->cartModel->getAll();
+        include_once('./views/cart/index.php');
+    }
+
+    public function actionAdd()
+    {
+
+        $title = 'Корзина';
+        $items = $this->cartModel->getAll();
+        echo 'hhhh';
+        if (isset($_POST['product_id'])) {
+            $id = htmlentities($_POST['product_id']);
+            $this->cartModel->addOrder($id);
+
+            //header('Location:  ' . FULL_SITE_ROOT . 'categories');
+        }
+        include_once('./views/cart/index.php');
+    }
+
+    public function actionEdit($id)
+    {
+
+
+    }
+
+
+}
